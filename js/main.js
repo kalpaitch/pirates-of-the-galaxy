7a6906b4cfa74f8ed0ed15c80c1e14751bb0089d
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2020-05-13 15:08:36 +0100

    Updated.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-$('#planets').masonry({
+const masonryOptions = {
     // options
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
     percentPosition: true
-});
+};
+let $container = $('#planets').masonry(masonryOptions);
 
 function playStory(element) {
+    const audioElements = document.getElementsByTagName('audio');
     const audio = document.getElementById(element);
-    audio.play();
+
+    const paused = audio.paused;
+
+    for(i=0; i<audioElements.length; i++) {
+        if (audio.currentSrc != audioElements[i].currentSrc) {
+            audioElements[i].pause();
+            audioElements[i].controls = false;
+        }
+    }
+
+    if (paused) {
+        audio.currentTime = 0;
+        audio.play()
+        audio.controls = true;
+    }
+    else {
+        audio.pause()
+        audio.controls = false;
+    }
+
+    $container.masonry('destroy');
+    $container.masonry(masonryOptions);
 }
